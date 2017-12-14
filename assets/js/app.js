17a01f3d1efbc69240a62c2432edd4b865a0eb27
ValeOlivares <valentinaolivares85@gmail.com>
2017-12-14 16:32:07 -0300

    agregando funcion número fono

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,13 @@ $(function(){
 });
 
 $("#demo").intlTelInput();
+
+$('#send').click(function() {
+  if ($('.phone').val().length != 10 || isNaN($('.phone').val())) {
+  $('.phone').css('border-color','#FF0000');
+   alert('El número de teléfono debe tener al menos 10 digitos.');
+     return false;
+   }else {
+    	
+        }
+    });
